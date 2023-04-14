f5b0479ac5c7c9e6e2fb7a2687072635714fe0e6
Arabi Alhumsi <arabi@codean.io>
2023-04-14 19:10:28 +0200

    fix: correct number and adjust padding top

--- layouts/ShowcaseLayout.tsx
+++ layouts/ShowcaseLayout.tsx
@@ -5,7 +5,7 @@ export { ShowcaseLayout };
 function ShowcaseLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="print:w-print paper:w-paper mx-auto w-full origin-top font-serif lg:scale-125 xl:scale-150 paper:py-20 print:scale-100">
-      <div className="paper:h-paper bg-white p-8 pt-10 shadow-none md:p-16 paper:shadow-2xl print:bg-white print:p-16 print:pt-20 print:shadow-none">
+      <div className="paper:h-paper bg-white p-8 pt-10 shadow-none md:p-16 md:pt-14 paper:shadow-2xl print:bg-white print:p-16 print:pt-20 print:shadow-none">
         {children}
       </div>
     </div>
