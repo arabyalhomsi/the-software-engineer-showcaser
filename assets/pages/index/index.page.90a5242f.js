1c2dbb4064c774622a5dd9da74bbb72b72e48cd7
arabyalhomsi <araby.ami@gmail.com>
2023-04-14 16:00:23 +0000

    Deploying to gh-pages from @ arabyalhomsi/the-software-engineer-showcaser@1028507375651289be252c77b558b5fc9bebffce 🚀

--- assets/pages/index/index.page.90a5242f.js
+++
@@ -1,1 +0,0 @@
-import{j as e,F as r}from"../../chunks/1979356d.js";function i(){return e(r,{children:e("div",{children:e("a",{href:"/resume",children:"Resume"})})})}export{i as Page};
---
+++ assets/pages/index/index.page.9be3ef9b.js
@@ -0,0 +1,1 @@
+import{r,j as e,F as t}from"../../chunks/1979356d.js";function a(){return r.useEffect(()=>{window.location.href="/resume"},[]),e(t,{})}export{a as Page};
