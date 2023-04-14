ce3a3e2cc72f59f99081e6f1e7967b8c013f41c8
arabyalhomsi <araby.ami@gmail.com>
2023-04-14 03:36:17 +0000

    Deploying to gh-pages from @ arabyalhomsi/the-software-engineer-showcaser@b374003e2e0d0f547cea953f33d17508a1af97e3 🚀

--- server/importBuild.cjs
+++
@@ -1,8 +0,0 @@
-{
-  const { setImportBuildGetters } = require('../../node_modules/vite-plugin-ssr/dist/cjs/node/runtime/globalContext/loadImportBuild.js');
-  setImportBuildGetters({
-    pageFiles: () => import('./pageFiles.mjs'),
-    clientManifest: () => require('../client/manifest.json'),
-    pluginManifest: () => require('../client/vite-plugin-ssr.json'),
-  });
-}
