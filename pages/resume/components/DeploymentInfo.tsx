504501f5d4525570eb6747ad7d70424a1fbe03fa
Arabi Alhumsi <arabi@codean.io>
2023-04-15 18:51:51 +0200

    Revert "fix: remove deployment data"
    This reverts commit 99d0bef144ce730d10f85b91b72f53c19779c479.

--- pages/resume/components/DeploymentInfo.tsx
+++ pages/resume/components/DeploymentInfo.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Icon } from '#root/components/Icon';
 
 export { DeploymentInfo };
 
 function DeploymentInfo() {
   return (
-    <div className="mt-10 flex flex-row items-center justify-center text-gray-600"></div>
+    <div className="mt-10 flex flex-row items-center justify-center text-gray-600">
+      <a
+        href="https://github.com/arabyalhomsi/the-software-engineer-showcaser/actions"
+        target="_blank"
+      >
+        <img
+          src="https://github.com/arabyalhomsi/the-software-engineer-showcaser/actions/workflows/deploy-github-pages.yml/badge.svg"
+          alt="deployment tag"
+          className="mx-3 h-5"
+        />
+      </a>
+      <a
+        href="https://github.com/arabyalhomsi/the-software-engineer-showcaser"
+        className="flex flex-row gap-2"
+      >
+        <span>View the source on Github</span>
+        <Icon src="github" width={12} height={12} />
+      </a>
+    </div>
   );
 }
--- pages/resume/components/SideTags.tsx
+++ pages/resume/components/SideTags.tsx
@@ -14,6 +14,13 @@ function SideTags() {
         <Icon src="printer" width={12} height={12} className="inline" />
         <h5 className="hidden lg:inline">Print This</h5>
       </SideTag>
+
+      <a className="tag" href="/">
+        <SideTag>
+          <Icon src="people" width={12} height={12} className="inline" />
+          <h5 className="hidden lg:inline">Check HoA</h5>
+        </SideTag>
+      </a>
     </div>
   );
 }
