fe0d9e5c4a2d2386a01ffe04b643d29d269f77c4
arabyalhomsi <araby.ami@gmail.com>
2025-02-25 11:45:26 +0000

    Deploying to gh-pages from @ arabyalhomsi/the-software-engineer-showcaser@5a2f8baffce9c0746afd7643ced49a2d57218ad0 🚀

---
+++ assets/entries/pages_index_index.page.be932764.js
@@ -0,0 +1,1 @@
+import{r,a as e,F as t}from"../chunks/chunk-1a4eff8b.js";function o(){return r.useEffect(()=>{window.location.href="/resume"},[]),e(t,{})}export{o as Page};
--- assets/pages/index/index.page.9be3ef9b.js
+++
@@ -1,1 +0,0 @@
-import{r,j as e,F as t}from"../../chunks/1979356d.js";function a(){return r.useEffect(()=>{window.location.href="/resume"},[]),e(t,{})}export{a as Page};
